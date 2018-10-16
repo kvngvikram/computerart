14670038d1eb20ffd630f475f783cc2bea77132b
happy <happykvng@gmail.com>
2018-10-16 22:10:31 +0530

    corrections in initial code at range swaping and new code for plotting with variations of p

--- mandelbrot_set_parameter_variations.py
+++ mandelbrot_set_parameter_variations.py
@@ -1,20 +1,20 @@
-xmin = -4	
+xmin = -2	
 xmax = 1.5
 ymin = -1.5 
 ymax = 1.5
 
 iterations = 30		# positive integer
-p = 2 			# parameter
+
+# parameter
+pmin = 2 
+pmax = 8 
+pres = 1+10 		# resolution in parameter vairations 
+
 threshold = 2 
 
-dynamic = True
-pausetime = 0.05 	# sec
+pausetime = 0.005 	# sec
 
 xresolution = 1+2000
-
-#Note: spyder users need to enter the following command(without '#') before running the code 
-# %matplotlib auto
-
 # yresolution can be set such that spaceing is same as of x or custiom can be commented out
 yresolution = 1+ int( (xresolution-1)*abs(ymax-ymin)/abs(xmax-xmin) )
 #yresolution = 1 + 1000
@@ -22,12 +22,11 @@ yresolution = 1+ int( (xresolution-1)*abs(ymax-ymin)/abs(xmax-xmin) )
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-
 # swaping maximum and minimum if correct values are not entered 
 if xmax<xmin:
 	xmax,xmin = xmin,xmax
 if ymax<ymin:
-	ymax,ymin = ymin,ymax
+	ymax,ymin = yim,ymax
 
 # Trying to make a complex plane
 x = np.linspace(xmin,xmax,xresolution)
@@ -43,59 +42,45 @@ z = c*0
 def iter(z,c,p):
 	return z**p+c
 
-# while loop for iterations
 plt.figure(0)
-i = 0
-while i < iterations : 
-	z = iter(z,c,p)
-	i = i+1
+
+parameterarray = np.linspace(pmin,pmax,pres)
+
+for p in parameterarray:
 	
-	if dynamic:	
-		zabs = np.absolute(z) 		# calculating the absolute value
-		nanflag = np.isnan(zabs)	# checking for infinite values (NaN)
-		thresholdflag = (zabs>threshold)# if absolute value threshold is reached
-		# NaN values are considered to be greater than threshold
-		flag = np.logical_or(nanflag,thresholdflag) 
-		flag = np.logical_not(flag) 	# this is flag for the set
-		
-		# first term :- NaN values in zabs are set max possible value
-		# second term:- non NaN values and not in set values are taken unaltered. Yet nan_to_num is used to remove NaN values. nan*0 = nan 
-		# therd term :- terms in set are assigned one and later log(1) = 0 while plotting			
-		m = np.nan_to_num(np.inf)*nanflag.astype(float) + np.nan_to_num(zabs)*(1-nanflag.astype(float))*(1-flag.astype(float)) + flag.astype(float)
+	z = c*0 
 
-		plt.clf()
-		# doing a log plot 
-		plt.imshow(np.log(m),extent=[xmin,xmax,ymin,ymax])
+	# while loop for iterations
+	i = 0
+	while i < iterations : 
+		z = iter(z,c,p)
+		i = i+1
 		
-		# for boolean plot of set comment above plot statement and uncomment below 
-		#plt.imshow(flag.astype(float),extent=[xmin,xmax,ymin,ymax])
-		plt.xlabel("Real axis")
-		plt.ylabel("Complex axis")
-		plt.title("iteration number :"+str(i))
-		plt.show(block=False)
-		plt.pause(pausetime)
-
-if not dynamic:
 	zabs = np.absolute(z) 		# calculating the absolute value
 	nanflag = np.isnan(zabs)	# checking for infinite values (NaN)
 	thresholdflag = (zabs>threshold)# if absolute value threshold is reached
 	# NaN values are considered to be greater than threshold
 	flag = np.logical_or(nanflag,thresholdflag) 
 	flag = np.logical_not(flag) 	# this is flag for the set
-
+	
 	# first term :- NaN values in zabs are set max possible value
 	# second term:- non NaN values and not in set values are taken unaltered. Yet nan_to_num is used to remove NaN values. nan*0 = nan 
 	# therd term :- terms in set are assigned one and later log(1) = 0 while plotting			
 	m = np.nan_to_num(np.inf)*nanflag.astype(float) + np.nan_to_num(zabs)*(1-nanflag.astype(float))*(1-flag.astype(float)) + flag.astype(float)
-	
 	plt.clf()
-	# doing a log plot
+	# doing a log plot 
 	plt.imshow(np.log(m),extent=[xmin,xmax,ymin,ymax])
 	
 	# for boolean plot of set comment above plot statement and uncomment below 
 	#plt.imshow(flag.astype(float),extent=[xmin,xmax,ymin,ymax])
 	plt.xlabel("Real axis")
 	plt.ylabel("Complex axis")
-	plt.title("iteration number :"+str(i))
+	plt.title("parameter value :"+str(p))
+	plt.show(block=False)
+	plt.pause(pausetime)
+
+
+
+
 
 plt.show()
