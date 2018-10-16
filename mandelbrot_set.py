3962d04f229ce56763e94fb8a5961f474fc19dbf
happy <happykvng@gmail.com>
2018-10-16 16:02:27 +0530

    Dynamic plotting

--- mandelbrot_set.py
+++ mandelbrot_set.py
@@ -9,6 +9,7 @@ iterations = 40		# positive integer
 p = 2 			# parameter
 threshold = 2 
 
+
 import numpy as np 
 import matplotlib.pyplot as plt 
 
@@ -28,15 +29,26 @@ def iter(z,c,p):
 	return z**p+c
 
 # while loop for iterations
+plt.figure(0)
 i = 0
 while i < iterations : 
 	z = iter(z,c,p)
 	i = i+1
+	
+	zabs = np.absolute(z) 		# calculating the absolute value
+	nanflag = np.isnan(zabs)
+	thresholdflag = (zabs>threshold)
+	flag = np.logical_or(nanflag,thresholdflag)
+	
+	plt.clf()
+	plt.imshow(flag.astype(float),extent=[xmin,xmax,ymin,ymax])
+	plt.xlabel("Real axis")
+	plt.ylabel("Complex axis")
+	plt.title("iteration number :"+str(i))
+	plt.show(block=False)
+	plt.pause(0.1)
+	
 
-zabs = np.absolute(z) 		# calculating the absolute value
-nanflag = np.isnan(zabs)
-thresholdflag = (zabs>threshold)
-flag = np.logical_or(nanflag,thresholdflag)
 
 plt.imshow(flag.astype(float),extent=[xmin,xmax,ymin,ymax])
 plt.show()
