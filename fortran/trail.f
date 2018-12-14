f7849237e1d2805765ae12dd19b20a1d594df59a
kvngvikram <happykvng@gmail.com>
2018-12-14 18:53:35 +0530

    starting with fortran

--- fortran/trail.f
+++ fortran/trail.f
@@ -2,9 +2,23 @@ program trying_complex
 
 implicit none
 
-complex :: cc(10)
+integer :: p=1 , q=3 , i=1 , j=1  
+complex :: cc(2,3)
+integer :: a(10) 
+
+
+do i = p,q
+  print*,i
+end do
+
+print*, cc*0+1 
+
+do i = p,q 
+  do j = p,q
+    cc(i,j) = cmplx(i,j)
+  end do
+end do        
 
 print*, cc 
-
 
 end program trying_complex
