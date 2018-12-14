cbafd62836120051d7285a04dc1adfcd37918bfd
kvngvikram <happykvng@gmail.com>
2018-12-14 20:09:28 +0530

    doing

--- fortran/trail.f
+++ fortran/trail.f
@@ -2,20 +2,19 @@ program trying_complex
 
 implicit none
 
-integer :: p=1 , q=3 , i=1 , j=1  
+integer :: min_x =-3 , max_x = 1 
+integer :: min_y =-2, max_y =4
+integer :: p = 2 , q = 3
 complex :: cc(2,3)
-integer :: a(10) 
 
+integer :: i=1 , j=1  
 
-do i = p,q
-  print*,i
-end do
 
 print*, cc*0+1 
 
-do i = p,q 
-  do j = p,q
-    cc(i,j) = cmplx(i,j)
+do i = 1,p
+  do j = 1,q
+    cc(i,j) = cmplx(j,i)
   end do
 end do        
 
