c9e83692ba2abc30b69613eb2e6ade09d872ccbe
kvng vikram <happykvng@gmail.com>
2018-12-18 11:47:27 +0530

    learning

---
+++ cpp/try.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<complex>
+
+using namespace std;
+int main(){
+	cout<<endl<<endl;
+	cout<<"hello world"<<endl;
+
+	complex<double> myvar = (1,3);
+	cout<<"number is : "<<myvar+complex<double>(1,2)<<endl;
+	
+	cout<<endl<<endl;
+	return 0;
+}
