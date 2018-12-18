1887a5d914a9afb4a3baff96b0bf2eb82901ab7f
kvng vikram <happykvng@gmail.com>
2018-12-18 21:26:30 +0530

    prepare_z function written

--- cpp/mandelbrot.cpp
+++ cpp/mandelbrot.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 void my_product(long double* pc , long double* pa , long double* pb , unsigned int* pxres , unsigned int* pyres ); 
 void input_check(float* pxmin , float* pxmax , float* pymin , float* pymax , unsigned int* pxres , unsigned int* pyres , long double* pzr , long double* pzi) ; 
-void prepare(long double* pc , unsigned int* pxres , unsigned int* pyres , float* pxmin , float* pxmax , float* pymin , float* pymax);	
+void prepare_c(long double* pc , unsigned int* pxres , unsigned int* pyres , float* pxmin , float* pxmax , float* pymin , float* pymax);	
+void prepare_z(long double* pz , unsigned int* pxres , unsigned int* pyres , long double* pzr , long double* pzi );	
 
 int main(){
 
@@ -14,23 +15,20 @@ int main(){
 	float ymax = 2  ;
 	unsigned int xres = 3 ; 
 	long double zr = 0 ;
-	long double zi  = 0 ; 
+	long double zi = 0 ; 
 	unsigned int yres; 
 	
 	input_check( &xmin , &xmax , &ymin , &ymax , &xres , &yres , &zr , &zi);
-	
 
 	yres = float(xres)*(ymax-ymin)/(xmax-xmin);
 	cout<<"\tThe range is"<<'\t'<<xmin<<'\t'<<xmax<<'\t'<<ymin<<'\t'<<ymax<<endl;	
 	cout<<"\tThe resolutions for x and y are "<<xres<<" and "<<yres<<endl;
 
-	long double c[yres][xres][2] ;
-	long double* pc = &c[0][0][0] ;
-	
+	long double c[yres][xres][2]  , z[yres][xres][2] ;
+	long double* pc = &c[0][0][0] , * pz = &z[0][0][0];
 
-	prepare(pc , &xres, &yres , &xmin , &xmax , &ymin , &ymax);
-
-
+	prepare_c(pc , &xres , &yres , &xmin , &xmax , &ymin , &ymax);
+	prepare_z(pz , &xres , &yres , &zr , &zi );
 	cout<<endl<<endl;
 	for(unsigned int j = 0 ;  j < yres ; j++ ){
 		for(unsigned int i = 0 ; i < xres ; i++ ){
@@ -52,12 +50,7 @@ int main(){
 
 
 void my_product(long double* pc , long double* pa , long double* pb , unsigned int* pxres , unsigned int* pyres ) { 
-/*	
-	long double ar , ai , br , bi , cr , ci ;
-	cr = ar*br - ai*bi; 
-	ci = ar*bi + br*ai; 
-	return cr , ci;
-*/
+	
 	unsigned int xres = *pxres , yres = *pyres ; 
 	long double a[yres][xres][2] , b[yres][xres][2] , c[yres][xres][2] ; 
 	
@@ -70,12 +63,11 @@ void my_product(long double* pc , long double* pa , long double* pb , unsigned i
 			ai = *(pa + xres*2*j + 2*i + 1); 
 			br = *(pb + xres*2*j + 2*i + 0);
 			bi = *(pb + xres*2*j + 2*i + 1); 
-			
 		//	cout<<"a is "<<'\t'<<ar<<'\t'<<ai<<endl;
 		//	cout<<"b is "<<'\t'<<br<<'\t'<<bi<<endl;
+		
 			cr = ar*br - ai*bi; 
 			ci = ar*bi + ai*br;
-			
 		//	cout<<"c is "<<'\t'<<cr<<'\t'<<ci<<endl;
 	
 			*(pc + xres*2*j + 2*i + 0) = cr; 
@@ -88,7 +80,6 @@ void my_product(long double* pc , long double* pa , long double* pb , unsigned i
 void input_check(float* pxmin , float* pxmax , float* pymin , float* pymax , unsigned int* pxres , unsigned int* pyres , long double* pzr , long double* pzi) {
 
 	cout<<"\tchecking "<<endl;
-	//cout<<"\tThe range is"<<'\t'<<*pxmin<<'\t'<<*pxmax<<'\t'<<*pymin<<'\t'<<*pymax<<endl;
 
 	if (*pymin>*pymax){
 		*pymax = *pymax + *pymin ; 
@@ -107,7 +98,7 @@ void input_check(float* pxmin , float* pxmax , float* pymin , float* pymax , uns
 	}
 }
 
-void prepare(long double* pc , unsigned int* pxres , unsigned int* pyres , float* pxmin , float* pxmax , float* pymin , float* pymax){	
+void prepare_c(long double* pc , unsigned int* pxres , unsigned int* pyres , float* pxmin , float* pxmax , float* pymin , float* pymax){	
 	unsigned int xres = *pxres , yres = *pyres ;
 	float xmin = *pxmin , xmax = *pxmax , ymin = *pymin , ymax = *pymax ; 
 	cout<<"\tThe range is"<<'\t'<<xmin<<'\t'<<xmax<<'\t'<<ymin<<'\t'<<ymax<<endl;	
@@ -124,4 +115,23 @@ void prepare(long double* pc , unsigned int* pxres , unsigned int* pyres , float
 	}
 } 
 
-// pointer arthematic 
+void prepare_z(long double* pz , unsigned int* pxres , unsigned int* pyres , long double* pzr , long double* pzi ) {	
+	
+	unsigned int xres = *pxres , yres = *pyres ;
+	float zr =  *pzr , zi = *pzi ;
+
+	for(unsigned int j = 0 ;  j < yres ; j++ ){
+		for(unsigned int i = 0 ; i < xres ; i++ ){
+			// *(pc + xres*2*j + 2*i + 0)
+			*(pz + xres*2*j + 2*i + 0) = zr; 
+			*(pz + xres*2*j + 2*i + 1) = zi; 
+		//	cout<< *(pz + xres*2*j + 2*i + 0) ; 
+		//	cout<< '\t'<< *(pz + xres*2*j + 2*i + 1) << endl;
+		}
+	}
+	 
+}	
+/* pointer arthematic 
+for a matrix M[X][Y][Z]
+M[i][j][k] = *(&M[0][0][0]+(Y*Z*i+Z*j+k))
+*/
