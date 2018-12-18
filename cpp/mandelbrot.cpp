330aab94e5cafae15f4c94970264055738d76046
kvng vikram <happykvng@gmail.com>
2018-12-18 21:06:44 +0530

    corrected the POINTER ARTHEMATIC

--- cpp/mandelbrot.cpp
+++ cpp/mandelbrot.cpp
@@ -1,98 +1,94 @@
 
-float xmin = 1 ;
-float xmax = -3  ;
-float ymin = -1 ; 
-float ymax = 2  ;
-unsigned int xres = 100 ; 
-long double zr = 0 ;
-long double zi  = 0 ; 
-
-
-
 #include<iostream>
-
-unsigned int yres; 
-
 using namespace std;
 
-void my_product(long double * pc , long double * pa , long double * pb ) ; 
-void input_check(float * pxmin , float * pxmax , float * pymin , float * pymax , unsigned int * pxres , unsigned int * pyres , long double * pzr , long double * pzi) ; 
+void my_product(long double* pc , long double* pa , long double* pb , unsigned int* pxres , unsigned int* pyres ); 
+void input_check(float* pxmin , float* pxmax , float* pymin , float* pymax , unsigned int* pxres , unsigned int* pyres , long double* pzr , long double* pzi) ; 
+void prepare(long double* pc , unsigned int* pxres , unsigned int* pyres , float* pxmin , float* pxmax , float* pymin , float* pymax);	
 
 int main(){
 
-	input_check( &xmin , &xmax , &ymin , &ymax , &xres , &yres , &zr , &zi);
-
+	float xmin = 1 ;
+	float xmax = -3  ;
+	float ymin = -1 ; 
+	float ymax = 2  ;
+	unsigned int xres = 3 ; 
+	long double zr = 0 ;
+	long double zi  = 0 ; 
+	unsigned int yres; 
 	
+	input_check( &xmin , &xmax , &ymin , &ymax , &xres , &yres , &zr , &zi);
+	
+
 	yres = float(xres)*(ymax-ymin)/(xmax-xmin);
+	cout<<"\tThe range is"<<'\t'<<xmin<<'\t'<<xmax<<'\t'<<ymin<<'\t'<<ymax<<endl;	
 	cout<<"\tThe resolutions for x and y are "<<xres<<" and "<<yres<<endl;
 
 	long double c[yres][xres][2] ;
-	long double * pc = &c[0][0][0] ;
-
-	my_product(pc,pc,pc); 
-
-
-
-
-
-
-
-
-/*
-	long double a[3][2]={{1,4},{3,5},{6,7}} , b[3]={1,2,3}  ; 
-	long double * pa = &a[0][0] , * pb = &b[0] ;
-	cout<<*(pa+1)<<endl;
-	long double * pc = myfunc( pa, pb);
-	cout<<*(pc+3)<<endl;
-
-
-	*(pc+3) = *(pc+3) + 2 ; 
-
-	cout<<*(pc+3)<<endl;
-	cout<<a[2][1]<<endl;
-
+	long double* pc = &c[0][0][0] ;
 	
-	//long double pc =  c ; 
-	//c  = my_complex_multiply(c,c);
-*/ 
+
+	prepare(pc , &xres, &yres , &xmin , &xmax , &ymin , &ymax);
+
+
+	cout<<endl<<endl;
+	for(unsigned int j = 0 ;  j < yres ; j++ ){
+		for(unsigned int i = 0 ; i < xres ; i++ ){
+			cout<< '\t'<<c[j][i][0]<<'\t'<<c[j][i][1] << endl;
+		}
+	}
+
+	my_product(pc,pc,pc,&xres,&yres);	
+
+	cout<<endl<<endl;
+	for(unsigned int j = 0 ;  j < yres ; j++ ){
+		for(unsigned int i = 0 ; i < xres ; i++ ){
+			cout<< '\t'<<c[j][i][0]<<'\t'<<c[j][i][1] << endl;
+		}
+	}
+
 }
 
 
 
-void my_product(long double * pc , long double * pa , long double * pb ) { 
+void my_product(long double* pc , long double* pa , long double* pb , unsigned int* pxres , unsigned int* pyres ) { 
 /*	
 	long double ar , ai , br , bi , cr , ci ;
 	cr = ar*br - ai*bi; 
 	ci = ar*bi + br*ai; 
 	return cr , ci;
 */
+	unsigned int xres = *pxres , yres = *pyres ; 
 	long double a[yres][xres][2] , b[yres][xres][2] , c[yres][xres][2] ; 
 	
 	long double ar , ai , br , bi , cr , ci ;
 
 	for(unsigned int j = 0 ;  j < yres ; j++ ){
 		for(unsigned int i = 0 ; i < xres ; i++ ){
-
-			ar = *(pa + i + j*i);
-			ai = *(pa + i + j*i + xres*yres); 
-			br = *(pb + i + j*i);
-			bi = *(pb + i + j*i + xres*yres);
+			// *(pc + xres*2*j + 2*i + 0)
+			ar = *(pa + xres*2*j + 2*i + 0);
+			ai = *(pa + xres*2*j + 2*i + 1); 
+			br = *(pb + xres*2*j + 2*i + 0);
+			bi = *(pb + xres*2*j + 2*i + 1); 
 			
+		//	cout<<"a is "<<'\t'<<ar<<'\t'<<ai<<endl;
+		//	cout<<"b is "<<'\t'<<br<<'\t'<<bi<<endl;
 			cr = ar*br - ai*bi; 
 			ci = ar*bi + ai*br;
 			
-			*(pc + i + j*i) = cr; 
-			*(pc + i + j*i + xres*yres) = ci;  
+		//	cout<<"c is "<<'\t'<<cr<<'\t'<<ci<<endl;
+	
+			*(pc + xres*2*j + 2*i + 0) = cr; 
+			*(pc + xres*2*j + 2*i + 1) = ci;  
 		}
 	} 	
 }
 
 
-void input_check(float * pxmin , float * pxmax , float * pymin , float * pymax , unsigned int * pxres , unsigned int * pyres , long double * pzr , long double * pzi) {
+void input_check(float* pxmin , float* pxmax , float* pymin , float* pymax , unsigned int* pxres , unsigned int* pyres , long double* pzr , long double* pzi) {
 
 	cout<<"\tchecking "<<endl;
-
-	cout<<"\tThe range is"<<'\t'<<*pxmin<<'\t'<<*pxmax<<'\t'<<*pymin<<'\t'<<*pymax<<endl;
+	//cout<<"\tThe range is"<<'\t'<<*pxmin<<'\t'<<*pxmax<<'\t'<<*pymin<<'\t'<<*pymax<<endl;
 
 	if (*pymin>*pymax){
 		*pymax = *pymax + *pymin ; 
@@ -109,7 +105,23 @@ void input_check(float * pxmin , float * pxmax , float * pymin , float * pymax ,
 		
 		cout<<"\tX range swapped"<<endl;	
 	}
-	
-	cout<<"\tThe range is"<<'\t'<<*pxmin<<'\t'<<*pxmax<<'\t'<<*pymin<<'\t'<<*pymax<<endl;
-	
+}
+
+void prepare(long double* pc , unsigned int* pxres , unsigned int* pyres , float* pxmin , float* pxmax , float* pymin , float* pymax){	
+	unsigned int xres = *pxres , yres = *pyres ;
+	float xmin = *pxmin , xmax = *pxmax , ymin = *pymin , ymax = *pymax ; 
+	cout<<"\tThe range is"<<'\t'<<xmin<<'\t'<<xmax<<'\t'<<ymin<<'\t'<<ymax<<endl;	
+	cout<<"\tThe resolutions for x and y are "<<xres<<" and "<<yres<<endl;
+	for(unsigned int j = 0 ;  j < yres ; j++ ){
+		for(unsigned int i = 0 ; i < xres ; i++ ){
+			
+			// *(pc + xres*2*j + 2*i + 0)
+			*(pc + xres*2*j + 2*i + 0) = xmin + (i)*(xmax-xmin)/float(xres-1); 
+			*(pc + xres*2*j + 2*i + 1) = ymax - (j)*(ymax-ymin)/float(yres-1); 
+		//	cout<< *(pc + xres*2*j + 2*i + 0) ; 
+		//	cout<< '\t'<< *(pc + xres*2*j + 2*i + 1) << endl;
+		}
+	}
 } 
+
+// pointer arthematic 
