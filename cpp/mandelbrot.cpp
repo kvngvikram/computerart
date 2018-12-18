1bf7738d58f5c1e326e6db42985d7532010ec15b
kvng vikram <happykvng@gmail.com>
2018-12-18 23:52:23 +0530

    removed add and multiply functions

--- cpp/mandelbrot.cpp
+++ cpp/mandelbrot.cpp
@@ -1,11 +1,12 @@
 
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-void my_product(long double* pc , long double* pa , long double* pb , unsigned int* pxres , unsigned int* pyres ); 
 void input_check(float* pxmin , float* pxmax , float* pymin , float* pymax , unsigned int* pxres , unsigned int* pyres , long double* pzr , long double* pzi) ; 
 void prepare_c(long double* pc , unsigned int* pxres , unsigned int* pyres , float* pxmin , float* pxmax , float* pymin , float* pymax);	
 void prepare_z(long double* pz , unsigned int* pxres , unsigned int* pyres , long double* pzr , long double* pzi );	
+void prepare_false_flag(bool* pf , unsigned int* pxres , unsigned int* pyres); 
 
 int main(){
 
@@ -15,67 +16,57 @@ int main(){
 	float ymax = 2  ;
 	unsigned int xres = 3 ; 
 	long double zr = 0 ;
-	long double zi = 0 ; 
+	long double zi = 0 ;
+	long double div_lim = 1000 ; 
+	unsigned int max_iter = 20 ; 
+ 
 	unsigned int yres; 
 	
 	input_check( &xmin , &xmax , &ymin , &ymax , &xres , &yres , &zr , &zi);
 
 	yres = float(xres)*(ymax-ymin)/(xmax-xmin);
-	cout<<"\tThe range is"<<'\t'<<xmin<<'\t'<<xmax<<'\t'<<ymin<<'\t'<<ymax<<endl;	
-	cout<<"\tThe resolutions for x and y are "<<xres<<" and "<<yres<<endl;
 
-	long double c[yres][xres][2]  , z[yres][xres][2] ;
+	long double c[yres][xres][2]  , z[yres][xres][2] , absz[yres][xres] ;
+	unsigned int itn[yres][xres] ; 
+	bool flag[yres][xres]  , flag2[yres][xres] ; 
+
 	long double* pc = &c[0][0][0] , * pz = &z[0][0][0];
-
 	prepare_c(pc , &xres , &yres , &xmin , &xmax , &ymin , &ymax);
 	prepare_z(pz , &xres , &yres , &zr , &zi );
-	cout<<endl<<endl;
-	for(unsigned int j = 0 ;  j < yres ; j++ ){
-		for(unsigned int i = 0 ; i < xres ; i++ ){
-			cout<< '\t'<<c[j][i][0]<<'\t'<<c[j][i][1] << endl;
-		}
-	}
+	prepare_false_flag( &flag[0][0] , &xres , &yres);
+	prepare_false_flag( &flag2[0][0] , &xres , &yres);
 
-	my_product(pc,pc,pc,&xres,&yres);	
-
-	cout<<endl<<endl;
-	for(unsigned int j = 0 ;  j < yres ; j++ ){
-		for(unsigned int i = 0 ; i < xres ; i++ ){
-			cout<< '\t'<<c[j][i][0]<<'\t'<<c[j][i][1] << endl;
+	 
+	//my_product(pc,pc,pc,&xres,&yres);
+	//my_add(pz,pz,pc,&xres,&yres);
+	
+	//cout<<endl<<endl;
+	
+	unsigned int n = 0 ; 
+	while ( n < max_iter){
+		for(unsigned int j = 0 ;  j < yres ; j++ ){
+				for(unsigned int i = 0 ; i < xres ; i++ ){	
+			//	z = z^2 + c 
+				z[j][i][0] = pow( z[j][i][0] , 2) - pow( z[j][i][1] , 2) + c[j][i][0]; 
+				z[j][i][1] = 2*z[j][i][0]*z[j][i][1] + c[j][i][0];
+				absz[j][i] = pow( ( pow(z[j][i][0] , 2) + pow(z[j][i][1] ,2) ) , 0.5)*float( ! flag[j][i]) + absz[j][i]*float(flag[j][i]);
+				
+				flag[j][i] = absz[j][i] > div_lim ; 
+				
+				itn[j][i] = itn[j][i]*float( flag[j][i] )  + n*float( ! flag[j][i] )  ;  	
+				
+				cout<<flag[j][i]; 
+			}
 		}
+		n++;
+		cout<<endl;
 	}
+	
+	cout<<endl<<endl;
 
 }
 
-
-
-void my_product(long double* pc , long double* pa , long double* pb , unsigned int* pxres , unsigned int* pyres ) { 
-	
-	unsigned int xres = *pxres , yres = *pyres ; 
-	long double a[yres][xres][2] , b[yres][xres][2] , c[yres][xres][2] ; 
-	
-	long double ar , ai , br , bi , cr , ci ;
-
-	for(unsigned int j = 0 ;  j < yres ; j++ ){
-		for(unsigned int i = 0 ; i < xres ; i++ ){
-			// *(pc + xres*2*j + 2*i + 0)
-			ar = *(pa + xres*2*j + 2*i + 0);
-			ai = *(pa + xres*2*j + 2*i + 1); 
-			br = *(pb + xres*2*j + 2*i + 0);
-			bi = *(pb + xres*2*j + 2*i + 1); 
-		//	cout<<"a is "<<'\t'<<ar<<'\t'<<ai<<endl;
-		//	cout<<"b is "<<'\t'<<br<<'\t'<<bi<<endl;
-		
-			cr = ar*br - ai*bi; 
-			ci = ar*bi + ai*br;
-		//	cout<<"c is "<<'\t'<<cr<<'\t'<<ci<<endl;
-	
-			*(pc + xres*2*j + 2*i + 0) = cr; 
-			*(pc + xres*2*j + 2*i + 1) = ci;  
-		}
-	} 	
-}
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void input_check(float* pxmin , float* pxmax , float* pymin , float* pymax , unsigned int* pxres , unsigned int* pyres , long double* pzr , long double* pzi) {
 
@@ -135,3 +126,12 @@ void prepare_z(long double* pz , unsigned int* pxres , unsigned int* pyres , lon
 for a matrix M[X][Y][Z]
 M[i][j][k] = *(&M[0][0][0]+(Y*Z*i+Z*j+k))
 */
+
+void prepare_false_flag(bool* pf , unsigned int* pxres , unsigned int* pyres){
+	unsigned int xres = *pxres , yres = *pyres;
+	for(unsigned int j = 0 ;  j < yres ; j++ ){
+		for(unsigned int i = 0 ; i < xres ; i++ ){
+			*(pf + xres*j + i + 0) = false; 
+		}
+	}
+}
